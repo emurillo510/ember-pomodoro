79c904486bfe576281e2c8038b77cdb2873006d7
Elton Murillo <emurillo510@gmail.com>
2016-10-15 19:18:05 -0700

    adds mock data (should remove mirage from repo)

--- mirage/config.js
+++ mirage/config.js
@@ -11,6 +11,23 @@ export default function() {
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+  this.namespace = 'api';
+
+  this.get('/pomodoros', () => {
+    return {
+     data: [/*{
+        type: "pomodoro",
+        id: "1",
+        attributes: {
+          name: 'pomodoro #00',
+          status: 'open'
+        }
+      }*/]
+    };
+  });
+
+  this.post('/pomodoros');
+
 
   /*
     Shorthand cheatsheet:
