3b53e555c3db2c15e3cf27c8ebf9b09e4eacd37a
Elton Murillo <emurillo510@gmail.com>
2016-10-25 19:45:21 -0700

    Adds support for enter key to add pomodoros

--- app/pomodoros/new/controller.js
+++ app/pomodoros/new/controller.js
@@ -1,24 +1,38 @@
 import Ember from 'ember';
+import KeyboardShortcuts from 'ember-keyboard-shortcuts/mixins/route';
 
-export default Ember.Controller.extend({
+const ENTER_KEY_CODE = 13;
+
+export default Ember.Controller.extend(KeyboardShortcuts, {
     pomodoroName: "",
     actions: {
-        addPomodoro() {
-            
-            let name = this.get('pomodoroName');
-            let status = "open";
+        addPomodoro(event) {
+            let keyCode = event.keyCode;
 
-            let pomodoro = this.get('store').createRecord('pomodoro', {
-                name,
-                status 
-            });
-            
-            pomodoro.save().then(()=> {
-                this.set('pomodoroName', '');
-                this.transitionToRoute('index');
-            }).catch(()=> {
+            console.log('keyCode', keyCode);
 
-            });
+            if(keyCode == ENTER_KEY_CODE){
+                let name = this.get('pomodoroName');
+                let status = "open";
+
+                let pomodoro = this.get('store').createRecord('pomodoro', {
+                    name,
+                    status 
+                });
+                
+                pomodoro.save().then(()=> {
+                    this.set('pomodoroName', '');
+                    this.transitionToRoute('index');
+                }).catch(()=> {
+
+                });
+            }
+            
+        }
+    },
+    keyboardShortcuts: {
+        'enter': function(){
+            alert('hello');
         }
     }
 });
